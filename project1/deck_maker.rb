5e57f00e06fb52dc6c75d7e7b0fafedf7e3bec88
AConnman <connors.143@osu.edu>
2021-02-04 13:34:31 -0500

    1) Addressed issue in initialize function in which integers did not index correctly. 2)Added CheckCard method. 3)Added Shuffle function. 4)Added ReturnOne function

--- project1/deck_maker.rb
+++ project1/deck_maker.rb
@@ -1,29 +1,36 @@
+#Author: Andrew Connors
 class DeckMaker
-  numbers = [1,2,3]
-  color = %w[R G P]
-  shapes = %w[*, 0, ~]
-  shading = [' ', '<', '/']
+
+  $numbers = [1,2,3]
+  $color = %w[R G P]
+  $shapes = %w[*, 0, ~]
+  $shading = [' ', '<', '/']
 
 
-  var deck = Array.new(81)
+  $deck = Array.new(81)
   int cardsLeft
   attr_accessor :deck, :cardsLeft
   #Initialize the deck to have 81 unique cards.
   def initialize
-    int w = 0
-    int x = 0
-    int y = 0
-    int z = 0
-    int count = 0
+    w = 0
+    x = 0
+    y = 0
+    z = 0
+    count = 0
 
     while w < 3
+      x = 0
       while x < 3
+        y = 0
         while y < 3
+          z = 0
           while z < 3
+            #This line should import cards class
+            require_relative 'Cards'
             card = Cards.new
-            card.initialize numbers[w], color[x], shapes[y], shading[z]
+            card.initialize $numbers[w], $color[x], $shapes[y], $shading[z]
             z+=1
-            deck[count] = card
+            @deck[count] = card
             count+=1
           end
           y+=1
@@ -42,6 +49,23 @@ class DeckMaker
 
   #Check to see if a given card is in the deck
   def checkCard(number, color, shapes, shading)
+    x = 0
+    while x < 81
+      if deck[x].color == color && deck[x].numbers == number && deck[x].shapes == shapes && deck[x].shading == shading
+        return true
+      end
+      x+=1
+
+    end
+  end
+    #Shuffle deck
+  def shuffle
+      @deck.shuffle!
+  end
+
+  #Return first element in array
+  def returnOne
+    return @deck.shift
+  end
 
   end
-end
