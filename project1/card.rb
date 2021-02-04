fa8444c30205ac171ac2fa44629e7a099b636f5b
LewisChen24 <chen.8279@buckeyemail.osu.edu>
2021-02-04 11:00:57 -0500

    Change the way to comment

--- project1/card.rb
+++ project1/card.rb
@@ -1,19 +1,19 @@
-/* describe the possible attributes 
-in each category of number, color, shapes ,and shading. */
+# describe the possible attributes 
+# in each category of number, color, shapes ,and shading. 
 class Cards
   number=[1,2,3]
   color=['R','G','P']
   shapes=['*','0','~']
   shading=[' ','<','/'
           ]
-  /*create a accessor to access the variable. */
+  #create a accessor to access the variable. 
   attr_accessor :number, :color,:shapes, :shading
 
-  /* initialize the value of number, color, shading, and shapes.
-In this case, each variable only contain one element. 
-For exmaple, @number is one of (1,2,3)
-@color is one of (R,G,P) 
-and same for shading and shapes. */
+  # initialize the value of number, color, shading, and shapes.
+# In this case, each variable only contain one element. 
+#For exmaple, @number is one of (1,2,3)
+#@color is one of (R,G,P) 
+#and same for shading and shapes.
 def initialize(number, color,shading, shapes)
   @number=number
   @color=color
@@ -22,8 +22,8 @@ def initialize(number, color,shading, shapes)
 end
 end
 
-/*Utilize .sample(i) to randomly return a value from each array to create string of the card. */
+#Utilize .sample(i) to randomly return a value from each array to create string of the card. 
 def return_card
   "#{number.sample(1)} #{color.sample(1)} #{shading.sample(1)}, #{shapes.sample(1)}"
 end
-/*since somebody says there inbuild instrument can be used in some later version of Ruby, I'm not pretty sure if there will be any problem in grading. */
+#since somebody says there inbuild instrument can be used in some later version of Ruby, I'm not pretty sure if there will be any problem in grading. 
