7ddc7ec42c772cc9fc04373961359a7171926dcd
cromlish4 <cromlish.4@osu.edu>
2021-02-03 20:42:56 -0500

    Removing Extra files, so that both methods are now in card_parsing.rb.

--- project1/deckCheck.rb
+++
@@ -1,9 +0,0 @@
-# deckCheck takes in a card input and a deck to check if it is in the deck
-def deckCheck(card, deck)
-  cardFound = false
-  # Goes though entire deck and makes cardFound true if @card is found
-  deck.each { |cardTest| if card == cardTest
-                           cardFound=true
-                         end}
-  return cardFound
-end
