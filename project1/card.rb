4cadacb5f2ce69ff7959268b89d4966d48df0f18
Samiul Islam <csisamiul@gmail.com>
2021-02-05 11:50:07 -0500

    Changed attribute arrays and their getters to be static.

--- project1/card.rb
+++ project1/card.rb
@@ -1,11 +1,10 @@
 # describe the possible attributes 
 # in each category of number, color, shapes ,and shading. 
 class Cards
-  number=[1,2,3]
-  color=['R','G','P']
-  shapes=['*','0','~']
-  shading=[' ','<','/'
-          ]
+  @@numbers=[1,2,3]
+  @@colors=['R','G','P']
+  @@shapes=['*','0','~']
+  @@shadingTypes=["  ","<>","//"]
   #create a accessor to access the variable. 
   attr_reader :number, :color,:shapes, :shading
 
@@ -21,25 +20,24 @@ def initialize(number, color,shading, shapes)
   @shading=shading
 end
 
-#getter to get number
-def number
-  @number
+#getter to get numbers array
+def self.getNumbers
+  @@number
 end
 
-#getter to get color
-def color
-  @color
+#getter to get color array
+def self.getColor
+  @@color
 end
 
-#getter to get shapes
-def shapes
-  @shapes
+#getter to get shapes array
+def self.getShapes
+  @@shapes
 end
 
-#getter to get shading
-def shading
-  @shading
-end
+#getter to get shading array
+def getShadingTypes
+  @@shading
 end
 
 #Utilize .sample(i) to randomly return a value from each array to create string of the card. 
@@ -47,3 +45,4 @@ def return_card
   "#{number} #{color} #{shading}, #{shape}"
 end
 #since somebody says there inbuild instrument can be used in some later version of Ruby, I'm not pretty sure if there will be any problem in grading. 
+end
