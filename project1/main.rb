3b0f152b2a8b92402b145f8721f53b87377a49a1
Samiul Islam <csisamiul@gmail.com>
2021-02-09 16:40:25 -0500

    Added some clarifying comments. Re-arranged game rules to conform with line lengths in editors.

--- project1/main.rb
+++ project1/main.rb
@@ -28,8 +28,10 @@ class Main
       c1 = input.slice 0, 4 # Get the string that describes 1st card
       c2 = input.slice 5, 4 # Get the string that describes 2nd card
       c3 = input.slice 10, 4  # Get the string that describes 3rd card
-      if validCard?(c1) && validCard?(c2) && validCard?(c3)
 
+      # Check if the 3 input cards could be valid
+      if validCard?(c1) && validCard?(c2) && validCard?(c3)
+        # Check to make sure these cards exist on the board
         cardsTemp1 = Card.new((c1.slice 0, 1).to_i, (c1.slice 1, 1), (c1.slice 2, 1), (c1.slice 3, 1))
         cardsTemp2 = Card.new((c2.slice 0, 1).to_i, (c2.slice 1, 1), (c2.slice 2, 1), (c2.slice 3, 1))
         cardsTemp3 = Card.new((c3.slice 0, 1).to_i, (c3.slice 1, 1), (c3.slice 2, 1), (c3.slice 3, 1))
@@ -66,19 +68,19 @@ class Main
     end
   end
 
-  # Convert the input string to an array of card objects
-  # Note doesn't truly convert but finds the card with the right values in the deck.
+  # Convert the input string to an array of card objects that exist on the board
   def self.convert(input)
     c1 = input.slice 0, 4 # Get the string that describes 1st card
     c2 = input.slice 5, 4 # Get the string that describes 2nd card
     c3 = input.slice 10, 4  # Get the string that describes 3rd card
-      
+
     cards = Array.new(3)  # Create an array for 3 card objects
     # Initialize each card object
     cards[0] = Card.new((c1.slice 0, 1), (c1.slice 1, 1), (c1.slice 2, 1), (c1.slice 3, 1)) #changed
     cards[1] = Card.new((c2.slice 0, 1), (c2.slice 1, 1), (c2.slice 2, 1), (c2.slice 3, 1))
     cards[2] = Card.new((c3.slice 0, 1), (c3.slice 1, 1), (c3.slice 2, 1), (c3.slice 3, 1))
 
+    #Match card objects to cards on the board
     i = 0
     cards.each do |card|
       cards[i] = CardParsing.convertStringToCardOnBoard(card, @board.board)
@@ -95,14 +97,18 @@ class Main
   color (red, green, or purple)
   shading (solid, striped, open)
   shape (diamond, squiggle, oval)
-  Each possible combination of features (e.g. a card with 3 striped green diamonds) appears as a card precisely once in the deck.
-  A SET must be either all the same or all different in each individual feature. In other words, if 2 cards are the same and 1 card is different in any feature, then it is NOT a SET (e.g. if 2 are red and 1 is purple then it is NOT a SET).
-  In the game, the deck is shuffled and 12 cards are laid face up on the table. When a player finds a SET among these 12 cards, the SET will be removed and the player gets 1 point. Then 3 new cards from the deck will be added to replenish the cards on the table. If there is no SET on the table, 3 cards from the deck will be added one time until there is a SET.\n""" # Game rules
+  Each possible combination of features (e.g. a card with 3 striped green diamonds) appears as a card precisely once
+  in the deck. A SET must be either all the same or all different in each individual feature. In other words, if 2 cards
+  are the same and 1 card is different in any feature, then it is NOT a SET (e.g. if 2 are red and 1 is purple then it
+  is NOT a SET). In the game, the deck is shuffled and 12 cards are laid face up on the table. When a player finds a SET
+  among these 12 cards, the SET will be removed and the player gets 1 point. Then 3 new cards from the deck will be added
+  to replenish the cards on the table. If there is no SET on the table, 3 cards from the deck will be added one time until
+  there is a SET.\n""" # Game rules
   puts """Input instructions:
-  Number of shapes: '1', '2', '3'
-  Color: 'R', 'G', 'P' stand for red, green, purple respectively, Cap's ONLY!
-  Shading: ' ', '<', '/' represent open, solid, striped
-  Shape: '*', '0', '~' represent diamond, oval, squiggle respectively
+  Number of shapes: #{Card.getShapes}
+  Color: #{Card.getColors} stand for red, green, purple respectively, Cap's ONLY!
+  Shading: #{Card.getShadingTypes} represent open, solid, striped
+  Shape: #{Card.getShapes} represent diamond, oval, squiggle respectively
   Example input: \"1R 0 2G<* 3P/~\" means a card with 1 red open oval, a card with 2 green solid diamond, and a card with 3 purple striped squiggle.
   Notice: Please follow the order of number of shapes, color, shading, and shape to specify a card!\nType <exit> to exit\n\n"""  # Input format
 
@@ -129,9 +135,10 @@ class Main
         @board.replenish # Add 3 cards to the board
       end
     elsif input == "exit"
+      # Exit game
       break
     else
-      # Convert the input string to an array of cards and valuate the SET
+      # Convert the input string to an array of cards and evaluate the SET
       cards = convert input
       if CardParsing.setParser(cards)
         puts "You found a SET and got 1 point!"
@@ -145,8 +152,9 @@ class Main
     end
   end
   #Segment to check if player wants to play again
-  puts("Game over!\n")
-  puts("Play again? (Y or N)\n")
+  # Display board one last time to show score
+  @board.display
+  puts("Game over!\nPlay again? (Y or N)\n")
   playAgain = gets.delete!("\n")
   while playAgain != "y" && playAgain != "Y" && playAgain != "N" && playAgain != "n"
     puts("Error in input\n")
