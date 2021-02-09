7ae4e78272cb46d08a6fe9b879699ae3a7b84926
Samiul Islam <csisamiul@gmail.com>
2021-02-08 20:12:33 -0500

    board was being called but wasn't initialized. card array was called without having been made fixed formatting errors in card initializations, needed more ()'s

--- project1/main.rb
+++ project1/main.rb
@@ -2,7 +2,13 @@
 # main class
 # Eric Young
 
-class main
+class Main
+
+  require_relative 'card.rb'
+  require_relative 'board.rb'
+  require_relative 'CardParser.rb'
+
+  @board = BoardMaker.new
 
   # Check a string represents a single card
   def validCard?(card_string)
@@ -48,9 +54,14 @@ class main
       
     cards = Array.new(3)  # Create an array for 3 card objects
     # Initialize each card object
-    cards[0].initialize(c1.slice 0 1, c1.slice 1 1, c1.slice 2 1, c1.slice 3 1)
-    cards[1].initialize(c2.slice 0 1, c2.slice 1 1, c2.slice 2 1, c2.slice 3 1)
-    cards[2].initialize(c3.slice 0 1, c3.slice 1 1, c3.slice 2 1, c3.slice 3 1)
+    cards[0] = Card.new((c1.slice 0, 1), (c1.slice 1, 1), (c1.slice 2, 1), (c1.slice 3, 1)) #changed
+    cards[1] = Card.new((c2.slice 0, 1), (c2.slice 1, 1), (c2.slice 2, 1), (c2.slice 3, 1))
+    cards[2] = Card.new((c3.slice 0, 1), (c3.slice 1, 1), (c3.slice 2, 1), (c3.slice 3, 1))
+
+    i = 0
+    cards.each do |card|
+      cards[i] = CardParsing.convertStringToCardOnBoard(card, @board.board)
+    end
 
     return cards
   end
@@ -62,49 +73,49 @@ class main
   color (red, green, or purple)
   shading (solid, striped, open)
   shape (diamond, squiggle, oval)
-  Each possible combination of features (e.g. a card with 3 striped gree diamonds) appears as a card precisely once in the deck.
+  Each possible combination of features (e.g. a card with 3 striped green diamonds) appears as a card precisely once in the deck.
   A SET must be either all the same or all different in each individual feature. In other words, if 2 cards are the same and 1 card is different in any feature, then it is NOT a SET (e.g. if 2 are red and 1 is purple then it is NOT a SET).
   In the game, the deck is shuffled and 12 cards are laid face up on the table. When a player finds a SET among these 12 cards, the SET will be removed and the player gets 1 point. Then 3 new cards from the deck will be added to replenish the cards on the table. If there is no SET on the table, 3 cards from the deck will be added one time until there is a SET.\n""" # Game rules
   puts """Input instructions:
   Number of shapes: '1', '2', '3'
-  Color: 'R', 'G', 'P' stand for red, gree, purple respectively, Cap's ONLY!
+  Color: 'R', 'G', 'P' stand for red, green, purple respectively, Cap's ONLY!
   Shading: ' ', '<', '/' represent open, solid, striped
   Shape: '*', '0', '~' represent diamond, oval, squiggle respectively
   Example input: \"1R 0 2G<* 3P/~\" means a card with 1 red open oval, a card with 2 green solid diamond, and a card with 3 purple striped squiggle.
   Notice: Please follow the order of number of shapes, color, shading, and shape to specify a card!\n"""  # Input format
 
-  @@points = 0  # Initialize user points
-
-  while deck.cardLeft != 0
-    board.display
+  while @board.deck.cardsLeft != 0
+    @board.display
     
     puts "Please enter a SET or NONE if no SET exists:"
-    input = gets  # Get user input as a string
+    input = gets.delete!("\n")  # Get user input as a string and remove new line
 
     # Check if the input is valid
     while !checkInput(input)
       # Error messages will be printed by lower level methods
       puts "Please enter a SET or NONE if no SET exists:"
-      input = gets
+      input = gets.delete!("\n")
     end
 
     # Evaluate the input
     if input == "NONE"  # The player claims to find no SET
-      if board.hasSET # The board has a SET TODO: Need hasSET from the board class
+      if @board.hasSet # The board has a SET
         puts "There is a SET and you missed it."
       else  # The board doesn't have a SET
-        board.replenish # TODO: Need replenish method from the board class
+        board.replenish # Add 3 cards to the board
       end
     else
       # Convert the input string to an array of cards and valuate the SET
-      if setParser(convert(input))
-        @@points += 1
+      cards = convert input
+      if CardParsing.setParser(cards)
         puts "You found a SET and got 1 point!"
-        board.remove(cards) # TODO: Need remove method from the board class
-        board.replenish # TODO: Need replenish method from the board class
+        @board.remove(cards) # Remove old cards
+        @board.replenish # Add 3 more cards
+        @board.score += 1 # Increment the score
       else
-        puts "Not a SET. No point rewarded."
+        puts "Not a SET, minus 1 point"
+        @board.score -= 1
       end
+    end
   end
-
-  puts "Thanks for playing. Your final points: " + @@points + "\nWould you like to play again?"
+  end
