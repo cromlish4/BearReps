3e48dd92d1438094e1177ebd45de55de578883b9
LewisChen24 <chen.8279@buckeyemail.osu.edu>
2021-02-21 22:29:19 -0500

    Figure out some problem.

--- project2/card.js
+++ project2/card.js
@@ -4,41 +4,41 @@ class Card(){
     var colors=['R','G','P'];
     var shapes=['*','0','~'];
     var shadingTypes=["-","<","/"];
-}
 
   /* initialize the value of number, color, shading, and shapes.
 In this case, each variable only contain one element. 
 For exmaple, @number is one of (1,2,3)
 @color is one of (R,G,P) 
 and same for shading and shapes.*/
-private Card(var color, var shape, var fill, var num) {
+    
+private Card(var colors, var shapes, var shadingTypes, var numbers) {
         this.colors = colors;
         this.shapes = shapes;
         this.shadingTypes = shadingTypes;
         this.numbers = numbers;
     }
 
-#getter to get numbers array
-    var getNumber(){
+//getter to get numbers array
+    function getNumber(){
 	return this.numbers;
     }
 
-#getter to get color array
- var getcolors(){
+//getter to get color array
+ function  getcolors(){
      return this.colors;
  }
     
 
-#getter to get shapes array
- var getshapes(){
+//getter to get shapes array
+ function getshapes(){
      return this.shapes;
  }
-#getter to get shading array
- var getshadingTypes(){
+//getter to get shading array
+ function getshadingTypes(){
      return this.shadingTypes;
  }
-#return attributes formatted for printing purposes
+//return attributes formatted for printing purposes
 function return_card(){
     printf(this.number+ this.color+this.shadingTypes+this.shapes);
 }
-
+}
