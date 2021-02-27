0781b3744d29016d3e9e5086cd45f926a2c18189
cs-isamiul <csisamiul@gmail.com>
2021-02-26 22:58:19 -0500

    Added () to method calls

--- project2/CardParser.js
+++ project2/CardParser.js
@@ -5,7 +5,7 @@ Method 2 checks if a card is in a deck.*/
 
 //Import Card Class
 import * as Card from './Card.js';
-class CardParsing {
+export default class CardParsing {
     /* Method 1
     setParser takes in 3 cards and checks if it is a set or not
     returns false if cards are not in a set, true if in set.
@@ -25,37 +25,37 @@ class CardParsing {
         var card3 = cards[2];
 
         // Check for amount of shape
-        if(card1.shape == card2.shape && card1.shape == card3.shape) {
+        if(card1.shape() == card2.shape() && card1.shape() == card3.shape()) {
             outputValueShape = true;
-        }else if(card1.shape != card2.shape && card1.shape != card3.shape &&
-            card2.shape != card3.shape) {
+        }else if(card1.shape() != card2.shape() && card1.shape() != card3.shape() &&
+            card2.shape() != card3().shape) {
             outputValueShape = true;
         }
         //Otherwise false by default
 
         //Check for Color
-        if(card1.color == card2.color && card1.color == card3.color) {
+        if(card1.color() == card2.color() && card1.color() == card3.color()) {
                 outputValueColor=true;
-        }else if(card1.color != card2.color && card1.color != card3.color &&
-             card2.color != card3.color) {
+        }else if(card1.color() != card2.color() && card1.color() != card3.color() &&
+             card2.color() != card3.color()) {
             outputValueColor=true;
         }
         //Otherwise false by default
 
         //Check for amount of Number
-        if(card1.number == card2.number && card1.number == card3.number) {
+        if(card1.number() == card2.number() && card1.number() == card3.number()) {
             outputValueNumber = true;
-        }else if(card1.number != card2.number && card1.number != card3.number &&
-            card2.number != card3.number) {
+        }else if(card1.number() != card2.number() && card1.number() != card3.number() &&
+            card2.number() != card3.number()) {
             outputValueNumber = true
         }
         //Otherwise false by default
 
         //Check for Shade
-        if(card1.shading == card2.shading && card1.shading == card3.shading) {
+        if(card1.shading() == card2.shading() && card1.shading() == card3.shading()) {
             outputValueShade = true;
-        }else if(card1.shading != card2.shading && card1.shading != card3.shading &&
-            card2.shading != card3.shading) {
+        }else if(card1.shading() != card2.shading() && card1.shading() != card3.shading() &&
+            card2.shading() != card3.shading()) {
             outputValueShade=true;
         }
         //Otherwise false by default
@@ -76,8 +76,8 @@ class CardParsing {
         var cardFound = false;
         // Goes though entire deck and makes cardFound true if card is found
         for(let cardTest of board){
-            if (card.shape == cardTest.shape && card.color == cardTest.color &&
-                card.shading == cardTest.shading && card.number == cardTest.number){
+            if (card.shape() == cardTest.shape() && card.color() == cardTest.color() &&
+                card.shading() == cardTest.shading() && card.number() == cardTest.number()){
                 cardFound=true;
             }
         }
@@ -89,7 +89,7 @@ class CardParsing {
     Make sure to call boardCheck first */
     convertStringToCardOnBoard(input, board) {
         for(let boardCard of board){
-            if (boardCard.return_card == input.return_card){
+            if (boardCard.return_card() == input.return_card()){
                 return boardCard;
             }
         }
