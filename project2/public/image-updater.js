41f2ed75aaa338896bb5a7669923e6281ded954c
cs-isamiul <csisamiul@gmail.com>
2021-03-01 22:03:30 -0500

    changed cell formating from decimal to hex

--- project2/public/image-updater.js
+++ project2/public/image-updater.js
@@ -4,36 +4,31 @@ export default class imageUpdate {
     static updateAllImages(cards) {
         var i;
         for (i = 0; i < cards.length; i++) {
-		 //Select element and update image//
-		var cell = document.getElementById("cell" + i).children[0];
-
-		cell.src  = this.getImagePath(cards[i]);
-
-
-		console.log(cell.src);
-
+            //Select element and update image//
+            var cell = document.getElementById("cell" + i.toString(16)).children[0];
+            cell.src = this.getImagePath(cards[i]);
         }
     }
 
     static getImagePath(card) {
 
-	    var answer = "";
-	    var ext = ".png";
+        var answer = "";
+        var ext = ".png";
 
-	if (card.shading === "<") {
+        if (card.shading === "<") {
 
-		answer =  "http://localhost:8000/assets/Set_Cards/" + card.number + card.color + card.shape + "%3C" + ext;
-
-
-	}else{
-
-		answer =  "http://localhost:8000/assets/Set_Cards/" + card.number + card.color + card.shape + card.shading + ext;
-   
-
-	}
+            answer = "http://localhost:8000/assets/Set_Cards/" + card.number + card.color + card.shape + "%3C" + ext;
 
 
-	return answer;
+        } else {
+
+            answer = "http://localhost:8000/assets/Set_Cards/" + card.number + card.color + card.shape + card.shading + ext;
+
+
+        }
+
+
+        return answer;
 
 
     }
