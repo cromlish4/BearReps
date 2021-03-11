0a96e749e41f5b6e1927f2f5ba8ead87dab337fe
cs-isamiul <csisamiul@gmail.com>
2021-03-11 14:25:57 -0500

    For the time being I have made a version that works with the classes.osu.edu site. THIS IS NOT THE FINAL VERSION! The return will be the same in the final but for now this is just a version that works for the purposes of testing other functionality. classes.osu.edu makes a request to a url that is in json formatting. I take this json file and parse it into a ruby hashset. There are examples of how you would access information from this hashset. Again storing this into the database and displaying it onto pages will be the same in the final or similar because I'll just return a hashset with similar formatting. For now this is just a version for the other members to be able to test out their methods.

--- project3/app/helpers/scraper.rb
+++ project3/app/helpers/scraper.rb
@@ -1,46 +1,31 @@
-require 'nokogiri'
-require 'httparty'
-require 'byebug'
-require 'mechanize'
-require 'rubygems'
 require 'net/http'
 require 'uri'
 require 'json'
 
-def scraper(class_number)
+def scraper(query)
   # initial setup
-  url = "https://courses.osu.edu/psp/csosuct/EMPLOYEE/PUB/c/COMMUNITY_ACCESS.OSR_CAT_SRCH.GBL?"
-  mechanize = Mechanize.new
+  query = query.gsub(" ", "%20")
+  url = "https://content.osu.edu/v2/classes/search?q=#{query}&campus=col&term=1214&p=1"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  # Parse data into a hashset
+  parsed_data = JSON.parse(response)
+  # In all likelihood courses[0] is all you need for the query being searched
+  courses = parsed_data['data']['courses']
 
-  #get page
-  page = mechanize.get(url)
+  # Example of how to print out information
+  # Create keys for important information
+  # course_keys = %w[catalogNumber title shortDescription description maxUnits component campus courseId]
+  # section_keys = %w[classNumber section component instructionMode enrollmentStatus]
+  #
+  # courses.each do |n|
+  #   course_keys.each { |k| puts "#{k} : #{n['course'][k]}"}
+  #   section_keys.each { |k| puts "#{k} : #{n['sections'][0][k]}"}
+  #   puts "\n"
+  # end
 
-  #get fields and fill them out
-  forms = page.forms[0]
-  forms['OSR_CAT_SRCH_WK_CATALOG_NBR'] = class_number
-  forms['OSR_CAT_SRCH_WK_DESCR'] = 'Computer Science & Engineering'
-
-  #submit form
-  page = forms.submit
-  #=====problems start here. must somehow read POST request made, I'll work on this tomorrow
-
-  #read header
-  header = Hash.new
-
-  page.header.each do |n|
-    header.store(n[0], n[1])
-  end
-
-  #send POST request
-  uri = page.uri
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # VERIFY_PEER in production https://stackoverflow.com/questions/9336884/getting-a-connection-reset-by-peer-error-when-hitting-google-contacts-api
-  request = Net::HTTP::Post.new(uri.request_uri, header)
-  response = http.request(request)
-  puts response.body
+  return courses
 end
 
-puts 'Starting program'
-scraper('3901')
-puts 'Ending program'
+# Example of how you might call the program
+#scraper('cse 3901')
