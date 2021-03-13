d334bf206f482520d0f1ee96cfbd897fe0b45695
cs-isamiul <csisamiul@gmail.com>
2021-03-12 20:56:03 -0500

    Added in some formatting options to now search for terms and campuses. Also cleaned up the keys. I'm going to try to get the other site working but this should work for now. That or I need to figure out why this classes.osu.edu doesn't want to show any forms to mechanize.

--- project3/app/helpers/scraper.rb
+++ project3/app/helpers/scraper.rb
@@ -2,30 +2,55 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def scraper(query)
-  # initial setup
-  query = query.gsub(" ", "%20")
-  url = "https://content.osu.edu/v2/classes/search?q=#{query}&campus=col&term=1214&p=1"
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  # Parse data into a hashset
-  parsed_data = JSON.parse(response)
-  # In all likelihood courses[0] is all you need for the query being searched
-  courses = parsed_data['data']['courses']
+class Scraper
+  @@terms = { 'summer' => 1214, 'spring' => 1212 }
+  @@campuses = { 'columbus'=> 'col', 'marion'=> 'mrn', 'newark'=> 'nwk', 'lima'=> 'lma', 'wooster'=> 'wst', "mansfield"=> 'mns' }
+  @@course_keys = %w[subject catalogNumber shortDescription title term maxUnits component campus]
+  @@section_keys = %w[section classNumber courseId component instructionMode meetings enrollmentStatus startDate endDate]
 
-  # Example of how to print out information
-  # Create keys for important information
-  # course_keys = %w[catalogNumber title shortDescription description maxUnits component campus courseId]
-  # section_keys = %w[classNumber section component instructionMode enrollmentStatus]
-  #
-  # courses.each do |n|
-  #   course_keys.each { |k| puts "#{k} : #{n['course'][k]}"}
-  #   section_keys.each { |k| puts "#{k} : #{n['sections'][0][k]}"}
-  #   puts "\n"
-  # end
+  def self.scrape(query, term, campus)
+    # initial setup
+    query = query.gsub(" ", "%20")
+    url = "https://content.osu.edu/v2/classes/search?q=#{query}&campus=#{campus}&term=#{term}&p=1&subject=cse"
+    uri = URI(url)
 
-  return courses
+    response = Net::HTTP.get(uri)
+    # Parse data into a hashset
+    parsed_data = JSON.parse(response)
+
+    courses = parsed_data['data']['courses']
+    # Example of how to print out information
+    # Very likely the first element will have what you want for an exact search
+    # courses.each do |n|
+    #   #print out the course information
+    #   @@course_keys.each { |k| puts "#{k} : #{n['course'][k]}"}
+    #   puts "\n"
+    #   #print out each sections information
+    #   n['sections'].each { |s| @@section_keys.each { |k| puts "#{k} : #{s[k]}"}; puts "\n"}
+    #   #waits for user to press enter
+    #   gets
+    #   puts "\n"
+    # end
+
+    return courses
+  end
+
+  def self.get_terms
+    @@terms
+  end
+
+  def self.get_campuses
+    @@campuses
+  end
+
+  def self.get_course_keys
+    @@course_keys
+  end
+
+  def self.get_section_keys
+    @@section_keys
+  end
+
+  # Example of how you might call the program, just replace @@ with the methods
+  # scrape('cse 3901', @@terms['spring'], @@campuses['columbus'])
 end
-
-# Example of how you might call the program
-#scraper('cse 3901')
