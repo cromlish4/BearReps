952375a93d3b480ec0d299e6b09c3e8f6837cb12
cromlish4 <cromlish.4@osu.edu>
2021-03-15 18:09:48 -0400

    Added back email support. Email is too embedded to easily remove.

--- project3/app/models/user.rb
+++ project3/app/models/user.rb
@@ -2,14 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
-  # Via - https://stackoverflow.com/questions/9165843/devise-not-requiring-email/18017413#18017413
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-
+         :recoverable, :rememberable, :validatable
 end
