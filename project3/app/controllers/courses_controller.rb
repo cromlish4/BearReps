0edfaf3148947521725477720be4a3cbc4cb584f
cs-isamiul <csisamiul@gmail.com>
2021-03-21 22:01:46 -0400

    Can now search by term and course number

--- project3/app/controllers/courses_controller.rb
+++ project3/app/controllers/courses_controller.rb
@@ -1,9 +1,10 @@
 # Via - https://web.archive.org/web/20190322155638/http://it.anar8.ru:80/312
 class CoursesController < ApplicationController
 
-
   def index
-    @courses = Course.all.order("catalog_number DESC")
+    #@courses = Course.all.order("catalog_number DESC")
+    @term = set_term(params[:set_term])
+    @courses = search(params[:search])
   end
 
   def show
@@ -28,13 +29,30 @@ class CoursesController < ApplicationController
     end
   end
 
-
-
   private def my_params
     params.require(:course).permit(:title, :term, :units, :campus, :subject, :catalog_number)
   end
 
-  private def create_hash
+  private def search(search)
+    if search
+      list = Course.where("catalog_number like ?", "%#{search}%")
+      list.where("term like ?", "%#{@term}%")
+    else
+      Course.all
+    end
   end
 
+  private def set_term(term)
+    if term and term != ""
+      #check if there is a course with this term
+      courses = Course.where("term like ?", "%#{term}%")
+      if not courses.blank?
+        term
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
 end
--- project3/config/routes.rb
+++ project3/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :courses
-  root 'courses#search'
+  root 'courses#index'
 
   get 'form' => 'courses#form'
 end
