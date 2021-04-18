f6e235d08f407c63a8a33be780d17c96105794f2
cromlish4 <cromlish.4@osu.edu>
2021-04-18 17:35:13 -0400

    Admin User search is now in a good working state, still will update later. Now porting to other forms.

--- project4/app/controllers/admin_user_controller.rb
+++ project4/app/controllers/admin_user_controller.rb
@@ -1,4 +1,5 @@
 class AdminUserController < ApplicationController
+  before_action :authenticate_user!
   def edit
     @Users = User.find_by(nameDotNumber: params[:nameDotNumber])
   end
@@ -10,16 +11,6 @@ class AdminUserController < ApplicationController
   end
   # Patch for updating user
   def index
-    # @admin_edit
-    # str_year = params[:users][:year]
-    # {"_method"=>"patch",
-    #  "authenticity_token"=>"8RIXihsxo3Bb3XDVa9LkkHyMIFJnNlt2AjkXHYspyFPj2hthJ0H348pMWqpQ/PtpoM8XJw52EA+v+vCWbgB2mA==",
-    #  "user"=>{"year"=>"Junior", "User Type"=>"admin"},
-    #  "Verified"=>"1",
-    #  "nameNum"=>"buckeye.1",
-    #  "commit"=>"Update Entry",
-    #  "format"=>"21"}
-
     # params[:user][:year]
     @User_to_update = User.find_by(:nameDotNumber => params[:nameNum])
     @User_to_update.update(:year => params[:user][:year])
