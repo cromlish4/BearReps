c1df86382a02b182e233e5aa6ec67d177df504ce
Ryan O'Donovan <odonovan.21@osu.edu>
2021-04-19 19:40:11 -0400

    Created basic tables for applications and recommendations

--- project4/db/migrate/20210419184357_create_application.rb
+++ project4/db/migrate/20210419184357_create_application.rb
@@ -1,6 +1,12 @@
 class CreateApplication < ActiveRecord::Migration[6.0]
   def change
     create_table :applications do |t|
+      t.string :applicant
+      t.string :email
+      t.string :course
+      t.integer :section
+
+      t.timestamps
     end
   end
 end
