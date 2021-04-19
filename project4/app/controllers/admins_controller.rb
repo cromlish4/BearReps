c65c58403f5e279349c5fe89630608c32c958c9b
cromlish4 <cromlish.4@osu.edu>
2021-04-19 00:42:30 -0400

    Fixed the new routing page, so that graders and users can have editing and users dont break.

--- project4/app/controllers/admins_controller.rb
+++ project4/app/controllers/admins_controller.rb
@@ -66,7 +66,7 @@ class AdminsController < ApplicationController
 
 
     if params[:search]!=""
-      @show_users = search_users(User)
+      @show_users = search_db(User)
     else
       @show_users = User
     end
@@ -79,25 +79,61 @@ class AdminsController < ApplicationController
   end
 
   # Patch for updating user
-  def user_show
-    # @admin_edit
-    str_year = params[:users][:year]
-
-    redirect_to "/admin/users/show?nameDotNumber="+str_year
-    #params[:nameNum]
-  end
+  # def user_show
+  #   # @admin_edit
+  #   str_year = params[:users][:year]
+  #
+  #   redirect_to "/admin/users/show?nameDotNumber="+str_year
+  #   #params[:nameNum]
+  # end
 
   def scrape_show
 
   end
+  # page for routing update to database edit
+  def admins_all
+    sub_dir = params[:sub_dir]
+    # sub_dir is the place that we need to redirect to
+    if (sub_dir == 'users')
+      # Users Edit code to update database
+      #Data[1].split("/")
 
-
+      @User_to_update = User.find_by(:nameDotNumber => params[:nameNum])
+      @User_to_update.update(:year => params[:user][:year])
+      @User_to_update.update(:fname => params[:user][:fname])
+      # @User_to_update.update(:nameDotNumber => params[:user][:nameDotNumber])
+      @User_to_update.update(:lname => params[:user][:lname])
+      if params["Verified"]=="1"
+        @User_to_update.update(:verified => "true")
+      else
+        @User_to_update.update(:verified => "false")
+      end
+      if (params[:keep_user] == "false")
+        @User_to_update.update(:user_type => params[:user]["User Type"])
+      end
+      @User_to_update.save
+      redirect_to "/admin/users/show?nameDotNumber="+params[:nameNum]
+    end
+  end
 
   def users_edit
     @edit_users = User.find_by(nameDotNumber: params[:nameDotNumber])
 
   end
 
+  #Graders
+    def graders
+      @show_users = User.order(sort_column + " " + sort_direction)
+
+
+      if params[:search]!=""
+        @show_users = search_db(User)
+      else
+        @show_users = User
+      end
+      #redirect_to admin_users_path
+      #redirect_to admin_users_path
+    end
 
   private
 
@@ -112,7 +148,7 @@ class AdminsController < ApplicationController
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
   # Via https://medium.com/swlh/using-rails-and-html-to-view-sort-and-search-tables-fbf8a0543558
-  def search_users(db)
+  def search_db(db)
     return db.where(fname: params[:search]).or(db.where(lname: params[:search]))
 
   end
--- project4/config/routes.rb
+++ project4/config/routes.rb
@@ -37,14 +37,28 @@ Rails.application.routes.draw do
   get 'admin_new' => 'admins#new'
 
   get 'admin/verify' => 'admins#verify'
+  get 'admin/user/:nameDotNumber', to: 'admins#verify_redirect'
   get 'admin/home' => 'admins#admin_home'
   get 'admin/verify.css' => 'admins#verify.css'
+
+  # Admin Menu Updater
+  patch 'admins/all' => 'admins#admins_all'
+
+  # Admin User Menu
   get 'admin/users' => 'admins#users', :as => :admin_users
   get 'admin/users/show' => 'admin_user#show'
   get 'admin/users/edit' => 'admin_user#edit'
-  patch 'admins/all' => 'admin_user#index'
-  get 'admin/user/:nameDotNumber', to: 'admins#verify_redirect'
+  get 'admin/users/index' => 'admin_user#index'
+
+
+  # Admin Scraper Menu
   get 'admin/scraper' => 'admins#scrape_show'
+  # Admin Grader Menu
+  get 'admin/graders' => 'admins#graders'
+  get 'admin/graders/show' => 'admin_grader#show'
+  get 'admin/graders/edit' => 'admin_grader#edit'
+  # patch 'admins/all' => 'admin_user#index'
+
 
 
   #Edit admin page
