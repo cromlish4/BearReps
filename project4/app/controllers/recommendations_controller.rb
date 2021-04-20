13340068d17773f06337d7013d5679ee79605774
Ryan O'Donovan <odonovan.21@osu.edu>
2021-04-20 18:03:36 -0400

    Modified the Recommendations Controller

--- project4/app/controllers/recommendations_controller.rb
+++ project4/app/controllers/recommendations_controller.rb
@@ -11,6 +11,7 @@ class RecommendationsController < ApplicationController
   # wired to app/views/recommendations/show.html.erb by default (naming convention)
   # display a recommendation (if the application doesn't exist, redirect to the page for all the apps)
   def show
+    @recommendation = Recommendation.find(params[:id])
     if @recommendation.nil?
       redirect_to action: :index
     end
@@ -19,20 +20,27 @@ class RecommendationsController < ApplicationController
   # wired to app/views/recommendations/new.html.erb by default (naming convention)
   # that view GET a blank form for creating a new recommendation, submitting with POST
   def new
-    @recommendation = Application.new
+    @recommendation = Recommendation.new
   end
-end
+
 
 # POST a newly filled form to create a new recommendation
 def create
-  @recommendation = Recommendation.new(params[:id])
-  @recommendation.owner = Instructor.find_or_create_by(name: params[:applicant])  # TODO: the "name:" here may need to be adjusted to the correct attribute that specifies the student's name in the student model for the database
-  # the applicant's name is contained in the parameter hash which is passed to the controller when the applicant submits the form with their name
-  # Student.find(params[:name]) makes use of the applicant's name and links the form with the student with that name
+  if true
+  @recommendation = Recommendation.new()
+  @recommendation.nameDotNumber = params[:app][:comments]
+  @recommendation.course = params[:app][:course]
+
   if @recommendation.save
     redirect_to @recommendation, notice: "Successfully Saved!"
   else
     render new
+  end
+  else
+    redirect_to home_url
+    flash.alert = "No Recommendations available!"
+
+  end
   end
 
   # wired to app/views/recommendations/update.html.erb by default (naming convention)
@@ -42,12 +50,12 @@ def create
 
   # PUT the edited form to update a recommendation
   def update
-    if @recommendation.update(attributes)
-      redirect_to @recommendation, notice: "Successfully Updated!"
-    else
-      render edit
-    end
+    @recommendation_to_update = Recommendation.find(params[:id])
+    @recommendation_to_update.update(:nameDotNumber => params[:app][:nameDotNumber])
+    @recommendation_to_update.update(:course => params[:app][:course])
+    @recommendation_to_update.save
 
+    redirect_to home_url
 
   end
 
@@ -62,5 +70,4 @@ def create
   def set_application
     @application = Application.find(params[:id])
   end
-
 end
